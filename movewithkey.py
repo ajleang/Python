2ca298d7c3f236003592f66e716819636868e497
Surachet S <surachetbanbung@gmail.com>
2023-01-26 12:40:56 +0700

    change text to blue and white

--- movewithkey.py
+++ movewithkey.py
@@ -3,7 +3,7 @@ from graphics import *
 win = GraphWin("My Window",900,900)
 win.setBackground("black")
 msg = Text(Point(100,100),"Hello!")
-msg.setTextColor("red")
+msg.setTextColor("Blue")
 msg.draw(win)
 while True:
     p = win.getKey()
