51f62f31f27f6880625439011d5cabf129a65dca
Surachet S <surachetbanbung@gmail.com>
2023-01-26 14:42:44 +0700

    white to red

--- showkey.py
+++ showkey.py
@@ -3,7 +3,7 @@ from graphics import *
 win = GraphWin("My Window",900,900)
 win.setBackground("black")
 msg = Text(Point(100,100),"Hello!")
-msg.setTextColor("white")
+msg.setTextColor("red")
 msg.draw(win)
 while True:
     p = win.getKey()
